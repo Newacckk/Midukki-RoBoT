9901073f19ef89d6fa9579810d9114abb453711b
Newacckk <119103479+Newacckk@users.noreply.github.com>
2022-12-05 18:14:15 +0530

    Update routes.py

--- Midukki/functions/routes.py
+++ Midukki/functions/routes.py
@@ -3,6 +3,6 @@ from Midukki import Configs
 
 routes = web.RouteTableDef()
 
-@routes.get("/", allow_head=Configs.STOP_BOT)
+@routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Mo_Tech_YT")
+    return web.json_response("@Mo_Tech_YT")
